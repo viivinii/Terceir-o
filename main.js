51d42dd6284f8c82647a15109d94cd3932c63c46
Vinicius Eduardo <111357753+viivinii@users.noreply.github.com>
2024-03-25 08:18:10 -0300

    5

---
+++ main.js
@@ -0,0 +1,13 @@
+const botões = document.querySelectorAll(".botao");
+
+for(let i=0; i <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classlist.remove("ativo")
+        }
+
+        botoes[i].classlist.add("ativo");
+    }
+    console.log(i)
+}
